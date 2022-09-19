1b46efa4d7e21f0b80cb6b42ca9009563657fbe3
Erick Rodrigues <erick.prodrigues@ufpe.br>
2022-09-19 13:26:26 -0300

    update chess

--- chessTable.js
+++ chessTable.js
@@ -2,4 +2,25 @@
     escreva um programa que cria uma string que representa uma grade 8x8, usando novas linhas
     para separar os caracteres. A cada posição  da grade existe um espaço ou um caractere "#".
     Esses caracteres formam um tabuleiro de xadrez.
+
+    Passando essa string para o console.log, o resultado deverá ser parecido com:
+
+    # # # # 
+     # # # #
+    # # # # 
+     # # # #
+    # # # # 
+     # # # #
+    # # # # 
+     # # # #
+
+    Quando você tiver o programa que gere este padrão, defina a variável size = 8
+    e altere o programa para que ele funcione para qualquer size, a saída da grade de lergura e altura.
+
+    Dica:
+
+    A sequência pode ser construída iniciando vazia ("") e repetidamente adicionando caracteres.
+    O caractere para uma nova linha é \n
+
+    
 */
