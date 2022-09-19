1de042e87a3f83ae6ba64da9d274d5d8e83104de
Erick Rodrigues <erick.prodrigues@ufpe.br>
2022-09-19 11:04:08 -0300

    cria fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+/* Escreva um programa que imprima usando console.log() todos os números de 1 a 100 com duas exceções. 
+  Para números divisíveis por 3, imprima Fizz ao invés do número, e para números divisíveis por 5 (enão 3),
+  imprima Buzz.
+*/
+
+// Implementando a solução utilizando baby steps
+
+/* 1 - precisamos imprimir os números de 1 a 100;
+  2 - É necessário fazer as validações de divisibilidade por 3 e por 5 (não incluindo 3);
+*/
+
+
