404902f48ecc5bd94434bf5f429661bbfbc2178e
Erick Rodrigues <erick.prodrigues@ufpe.br>
2022-09-19 13:21:11 -0300

    cria chessTable ***fktrb***

---
+++ chessTable.js
@@ -0,0 +1,5 @@
+/* Tabuleiro de Xadrez
+    escreva um programa que cria uma string que representa uma grade 8x8, usando novas linhas
+    para separar os caracteres. A cada posição  da grade existe um espaço ou um caractere "#".
+    Esses caracteres formam um tabuleiro de xadrez.
+*/
